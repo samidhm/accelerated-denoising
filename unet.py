97a1e9c20494df8356893f235436e39960b247e1
Suguna Velury <sugunav@Sugunas-MacBook-Pro.local>
2024-05-30 14:50:41 -0700

    Parameterized number of encoder and decoder layers

--- unet.py
+++ unet.py
@@ -3,27 +3,25 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class UNet(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, no_layers = 4, bottleneck = "conv"):
         super(UNet, self).__init__()
 
-        # Define the encoder layers
-        self.enc1 = self.conv_block(in_channels=in_channels, out_channels=64)
-        self.enc2 = self.conv_block(in_channels=64, out_channels=128)
-        self.enc3 = self.conv_block(in_channels=128, out_channels=256)
-        self.enc4 = self.conv_block(in_channels=256, out_channels=512)
+        #Define the encoder layers
+        self.enc = nn.ModuleList()
+        self.enc.append(self.conv_block(in_channels=in_channels, out_channels=64))
+        for i in range(no_layers-1):
+            self.enc.append(self.conv_block(in_channels=2**(i+6), out_channels=2**(i+7)))
 
         # Define the bottleneck layer
-        self.bottleneck = self.conv_block(in_channels=512, out_channels=1024)
+        if bottleneck == "conv":
+            self.bottleneck = self.conv_block(in_channels=2**(no_layers+5), out_channels=2**(no_layers+6))
 
         # Define the decoder layers
-        self.upconv4 = self.upconv_block(in_channels=1024, out_channels=512)
-        self.dec4 = self.conv_block(in_channels=1024, out_channels=512)
-        self.upconv3 = self.upconv_block(in_channels=512, out_channels=256)
-        self.dec3 = self.conv_block(in_channels=512, out_channels=256)
-        self.upconv2 = self.upconv_block(in_channels=256, out_channels=128)
-        self.dec2 = self.conv_block(in_channels=256, out_channels=128)
-        self.upconv1 = self.upconv_block(in_channels=128, out_channels=64)
-        self.dec1 = self.conv_block(in_channels=128, out_channels=64)
+        self.dec = nn.ModuleList()
+        for j in range(no_layers, 0, -1):
+            self.dec.append(self.upconv_block(in_channels=2**(j+6), out_channels=2**(j+5)))
+            self.dec.append(self.conv_block(in_channels=2**(j+6), out_channels=2**(j+5)))
+
 
         # Define the final output layer
         self.final = nn.Conv2d(in_channels=64, out_channels=out_channels, kernel_size=1)
@@ -42,32 +40,22 @@ class UNet(nn.Module):
         return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
 
     def forward(self, x):
-        # Encoder path
-        enc1 = self.enc1(x)
-        enc2 = self.enc2(F.max_pool2d(enc1, 2))
-        enc3 = self.enc3(F.max_pool2d(enc2, 2))
-        enc4 = self.enc4(F.max_pool2d(enc3, 2))
+        enc_outputs = []
+        ip = x
+        for l in self.enc:
+            ip = l(ip)
+            enc_outputs.insert(0, ip)
+            ip = F.max_pool2d(ip, 2)
 
         # Bottleneck
-        bottleneck = self.bottleneck(F.max_pool2d(enc4, 2))
+        ip = self.bottleneck(ip)
 
-        # Decoder path
-        dec4 = self.upconv4(bottleneck)
-        dec4 = torch.cat((dec4, enc4), dim=1)
-        dec4 = self.dec4(dec4)
-        
-        dec3 = self.upconv3(dec4)
-        dec3 = torch.cat((dec3, enc3), dim=1)
-        dec3 = self.dec3(dec3)
-        
-        dec2 = self.upconv2(dec3)
-        dec2 = torch.cat((dec2, enc2), dim=1)
-        dec2 = self.dec2(dec2)
-        
-        dec1 = self.upconv1(dec2)
-        dec1 = torch.cat((dec1, enc1), dim=1)
-        dec1 = self.dec1(dec1)
+        for idx, l in enumerate(self.dec):
+            if idx%2 == 0:
+                ip = l(ip)
+                ip = torch.cat((ip, enc_outputs[idx // 2]), dim=1)
+            else:
+                ip = l(ip)
 
-        # Final output layer
-        out = self.final(dec1)
+        out = self.final(ip)
         return out
