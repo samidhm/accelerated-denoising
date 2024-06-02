f458d941dad4e7d285015f2e89fb1d1fed2c6168
thejas <thejasvenkatesh97@gmail.com>
2024-06-01 18:31:11 -0700

    Add feature ablation capability to dataset.py

--- dataset.py
+++ dataset.py
@@ -6,9 +6,15 @@ from torchvision import transforms
 import numpy as np
 
 class CustomImageDataset(Dataset):
-    def __init__(self, txt_file, data_path):
+    def __init__(self, txt_file, data_path, include=["depth", "normal", "relative_normal", "albedo", "roughness"]):
         self.data_path = data_path
         self.image_names = self._read_txt_file(txt_file)
+        self.include = include
+
+        self.num_features = 3
+
+        sizes = {"depth": 1, "normal": 3, "relative_normal": 3, "albedo": 3, "roughness": 3}
+        self.num_features += sum([sizes[x] for x in include])
 
     def _read_txt_file(self, txt_file):
         with open(txt_file, 'r') as file:
@@ -33,26 +39,31 @@ class CustomImageDataset(Dataset):
 
         # Load input images
         input_images = [self._load_image(f"{self.data_path}/samples_1", image_name)]
+
+        if "depth" in self.include:
+            input_images.append(self._load_image(f"{self.data_path}/depth", image_name, True))
         
-        input_images.append(self._load_image(f"{self.data_path}/depth", image_name, True))
-        input_images.append(self._load_image(f"{self.data_path}/normal", image_name))
+        normal_image = self._load_image(f"{self.data_path}/normal", image_name)
+        if "normal" in self.include:
+            input_images.append(normal_image)
 
-        view_matrix = np.load(f"{self.data_path}/view_space_matrix/{image_name[:-3]}npy")
-        view_matrix = torch.tensor(view_matrix, dtype=input_images[-1].dtype).reshape(1, 1, 3, 3)
+        if "relative_normal" in self.include:
+            view_matrix = np.load(f"{self.data_path}/view_space_matrix/{image_name[:-3]}npy")
+            view_matrix = torch.tensor(view_matrix, dtype=input_images[-1].dtype).reshape(1, 1, 3, 3)
 
-        relative_normal = input_images[-1].transpose(-1, -3)
-        current_shape = relative_normal.shape
-        relative_normal = relative_normal.reshape(*current_shape, 1)
-        relative_normal = (view_matrix @ relative_normal).reshape(*current_shape)
-        relative_normal = torch.nn.functional.normalize(relative_normal, dim=-1)
-        relative_normal = relative_normal.transpose(-1, -3) 
+            relative_normal = normal_image.transpose(-1, -3)
+            current_shape = relative_normal.shape
+            relative_normal = relative_normal.reshape(*current_shape, 1)
+            relative_normal = (view_matrix @ relative_normal).reshape(*current_shape)
+            relative_normal = torch.nn.functional.normalize(relative_normal, dim=-1)
+            relative_normal = relative_normal.transpose(-1, -3) 
 
-        assert relative_normal.shape == input_images[-1].shape
+            assert relative_normal.shape == normal_image.shape
+            input_images.append(relative_normal)
 
-        input_images.append(relative_normal)
-        
-        input_images.append(self._load_image(f"{self.data_path}/diffuse_color", image_name) + \
-                            self._load_image(f"{self.data_path}/glossy_color", image_name))
+        if "albedo" in self.include:
+            input_images.append(self._load_image(f"{self.data_path}/diffuse_color", image_name) + \
+                                self._load_image(f"{self.data_path}/glossy_color", image_name))
         
         
         # Concatenate input images along the channel dimension
--- test.py
+++ test.py
@@ -1,38 +1,58 @@
+import time
 import torch
 from torch.utils.data import DataLoader
 import numpy as np
 import os
 from PIL import Image
+import argparse
 
 from unet import UNet
 from dataset import CustomImageDataset
+
+from model_stats import SizeEstimator
 
 def create_dataset(test_txt, data_path, batch_size=32):
     test_dataset = CustomImageDataset(test_txt, data_path)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     return test_loader
 
-test_loader = create_dataset("data/test.txt", "data/raw_data")
+parser = argparse.ArgumentParser(description="UNet inference training script")
+
+parser.add_argument("-e", "--experiment", default="unet_denoising", type=str, help="Name of the experiment to evaluate")
+parser.add_argument("-b", "--batch_size", default=1024, type=int, help="Inference batch size")
+
+args = parser.parse_args()
+
+# TODO: Infer model size and dataset features
+test_loader = create_dataset("data/test.txt", "data/raw_data", args.batch_size)
 
 device = torch.device("cuda")
 model = UNet(13, 3).to(device)
 
-model.load_state_dict(torch.load('unet_denoising.pth'))
+model.load_state_dict(torch.load(f"{args.experiment}.pth"))
 model.eval()
 
-output_dir = 'denoised_test_images'
+output_dir = os.path.join('evaluation_results', args.experiment)
+images_dir = os.path.join(output_dir, args.experiment, "images")
 os.makedirs(output_dir, exist_ok=True)
+os.makedirs(images_dir, exist_ok=True)
 
 test_files = open("data/test.txt", "r").read().strip().split("\n")
+latencies = []
 
 with torch.no_grad():
     for idx, (noisy_image, _) in enumerate(test_loader):
         noisy_image = noisy_image.to('cuda')
+        
+        t = time.time()
         outputs = model(noisy_image)
+        latencies.append(time.time() - t)
+
         for i in range(outputs.size(0)):
             denoised_img = outputs[i].cpu().numpy().transpose(1, 2, 0)  # Convert to HWC format
             denoised_img = (denoised_img * 255).astype(np.uint8)  # Convert to uint8
             img = Image.fromarray(denoised_img)
-            img.save(os.path.join(output_dir, f'{test_files[idx * test_loader.batch_size + i]}'))
+            img.save(os.path.join(images_dir, f'{test_files[idx * test_loader.batch_size + i]}'))
         
+
 print(f"Denoised test images saved to {output_dir}")
