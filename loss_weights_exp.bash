c3103d909e234e0f1f19b015586820d4e441aec8
Suguna Velury <sugunav@DNa80d57c.SUNet>
2024-06-02 19:52:33 -0700

    Number of layers experiment bash script

--- loss_weights_exp.bash
+++ loss_weights_exp.bash
@@ -3,5 +3,5 @@
 # Iterate over the range of values for parameter -a
 for a in $(seq 0 0.1 1); do
     echo "Running command with value: $a"
-    python3 train.py -n 4 -b conv -f depth normal albedo -a $a -t sample_tag
+    python3 train.py -n 4 -b conv -f depth normal albedo relative_normal roughness -a $a -t sample_tag
 done
